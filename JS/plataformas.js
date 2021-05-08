22b31aa6d2623cdd892b4f32fa6c562376b7d114
unknown <larger13@hotmail.com>
2021-05-08 16:12:19 -0300

    magia negra

--- JS/plataformas.js
+++ JS/plataformas.js
@@ -64,7 +64,8 @@ function getInformacoesJogo(jogoClone) {
     jogo.nrEstrelasVazias = jogoClone.nrEstrelasVazias;
     jogo.urlVideo = jogoClone.urlVideo;
     jogo.caminhoImagemPrincipal = jogoClone.caminhoImagemPrincipal;
-    jogo.caminhoImagemSecundaria = jogoClone.caminhoImagemSecundaria;
+    jogo.listaImagens = jogoClone.listaImagens;
+    jogo.idJogo = jogoClone.idJogo;
 
     return jogo;
 }
@@ -85,7 +86,8 @@ function montarHtmlCards(listaJogosPorPlataforma) {
         card = card.replace("#vlrJogo#", listaJogosPorPlataforma[i].vlrJogo);
         card = card.replace("#estrelas#", montarEstrelas(listaJogosPorPlataforma[i]));
         card = card.replace("#imagem#", "style=\"background: url('" + listaJogosPorPlataforma[i].caminhoImagemPrincipal + "')no-repeat center top\"");
-       
+        card = card.replace("#idJogo#", listaJogosPorPlataforma[i].idJogo);
+      
         if (count < 2) {
             card = card.replace("#clasMargin#", "pr-50");
             count += 1;
@@ -122,4 +124,78 @@ function montarEstrelas(jogo) {
         estrelas += "<span class='far fa-star icon-star'></span>"
     }
     return estrelas
+}
+
+function showInformacoesJogo(idJogo) {
+    
+    $("#plataformas-jogos").hide();   
+
+    idJogo = parseInt(idJogo);
+
+    var jogoJson = getJogoPorId(idJogo);
+    
+    popularInformacoesJogo(jogoJson);
+
+    $("#detalhe-jogo").show();   
+
+}
+
+function popularInformacoesJogo(jogoJson) {
+
+    popularImagensAux(jogoJson);
+
+    popularImagemGrande(jogoJson.caminhoImagemPrincipal)
+
+    popularinformacoesDiversas(jogoJson);
+
+}
+
+function popularinformacoesDiversas(jogoJson) {
+
+    $("#nmJogoPreview").text(jogoJson.nmJogo)
+
+}
+
+function popularImagensAux(jogoJson) {
+
+    var htmlImagens = "";
+
+    for (var i = 0; i < jogoJson.listaImagens.length; i++) {
+        htmlImagens += "<img onclick='prepararImagemGrande(this)' src='" + jogoJson.listaImagens[i] + "' class='img-thumbnail imagem-aux mb-3 imagem-resolucao' alt='Imagem " + (i+1) + "'>";
+        
+        if (i == 3) {
+            break;
+        }
+    }
+
+    $("#listaImagensAux").html(htmlImagens);
+}
+
+function prepararImagemGrande(element) {
+
+    var nmCaminhoImagem = $(element).attr("src");
+    
+    popularImagemGrande(nmCaminhoImagem);
+}
+
+function popularImagemGrande(nmCaminhoImagem) {
+
+    $("#imagemGrande").html("<img src='" + nmCaminhoImagem + "' class='img-thumbnail imagem-grande mb-3 imagem-resolucao' alt='Imagem Preview'>");
+
+}
+
+function getJogoPorId(idjogo) {
+
+    var jogo = new Object();
+
+    for (var i =0; i < listaJogosJson.length; i++) {
+
+        if (listaJogosJson[i].idJogo == idjogo) {
+            jogo = listaJogosJson[i];
+            break;
+        }
+
+    }
+
+    return jogo;
 }
